c65f72a1f86013ead25f546de684a1b3bc398695
nmq512 <quan5122004@gmail.com>
2026-07-20 09:57:07 +0700

    First commit

---
+++ blogmanager_NguyenMinhQuan/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace blogmanager_NguyenMinhQuan.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
