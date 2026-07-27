6c4bf307b1950f3d7779d7a33c2d18899115b7e4
nmq512 <quan5122004@gmail.com>
2026-07-27 12:12:58 +0700

    Chỉnh sửa code LabController

--- blogmanager_NguyenMinhQuan/Controllers/LabController.cs
+++ blogmanager_NguyenMinhQuan/Controllers/LabController.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 public class LabController : Controller
 {
-    public IActionResult Index()
+    List<Post> getListPost()
     {
-        var baiViet = new List<Post>
+         var posts = new List<Post>
         {
             new Post
             {
@@ -28,35 +28,18 @@ public class LabController : Controller
                 Id = 5, Title = "ASP.NET Core", IsPublished = true, ViewCount=12, Author = "Nguyễn Minh Quân"
             }
         };
+        return posts;
+    }
+    public IActionResult Index()
+    {
+        var baiViet = getListPost();
         ViewBag.SoDaXuatBan = baiViet.Count(p => p.IsPublished);
         ViewBag.BaiViet = baiViet.Where(p => p.IsPublished).OrderBy(p => p.Title).ToList();
         return View();
     }
     public IActionResult CountView()
     {
-        var baiViet = new List<Post>
-        {
-            new Post
-            {
-                Id = 1, Title = "C# cơ bản", IsPublished = true, ViewCount=10, Author = "Nguyễn Phương"
-            },
-            new Post
-            {
-                Id = 2, Title = "MVC nhập môn", IsPublished = false, ViewCount=50, Author = "Nguyễn Trung"
-            },
-            new Post
-            {
-                Id = 3, Title = "EF Core", IsPublished = true, ViewCount=75, Author = "Đặng Anh Quân"
-            },
-            new Post
-            {
-                Id = 4, Title = "Blazor", IsPublished = false, ViewCount=30, Author = "Hồng Trung Việt"
-            },
-            new Post
-            {
-                Id = 5, Title = "ASP.NET Core", IsPublished = true, ViewCount=120, Author = "Nguyễn Minh Quân"
-            }
-        };
+        var baiViet = getListPost();
         ViewBag.TongLuotXem = baiViet.Sum(p => p.ViewCount);
         ViewBag.BaiViet = baiViet.Where(p => p.IsPublished).OrderBy(p => p.ViewCount).ToList();
         var baiVietNhieuViewNhat = baiViet.OrderByDescending(p => p.ViewCount).First();
